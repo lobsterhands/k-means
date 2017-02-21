338d70627fb3cc4e28b4d8005cf82db533b87e4d
Lyle <denmanlp@gmail.com>
2017-02-21 16:57:45 -0600

    Create KMeans class; read from input file; parse data and create Points which populate a point list

--- src/KMeans.java
+++ src/KMeans.java
@@ -1,34 +1,78 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.URL;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by Lyle on 2/21/2017.
  */
 public class KMeans {
+    private int k;
+    private URL fileURL;
+    private List<Point> pointList;
+    private List clusterList;
+
+    private KMeans(String k, String fileName) {
+        this.k = Integer.parseInt(k);
+        this.fileURL = ClassLoader.getSystemClassLoader().getResource(fileName);
+        this.pointList = new ArrayList<>();
+        this.clusterList = new ArrayList();
+    }
+
+    private String getFileString() {
+        return fileURL.getFile();
+    }
+
+    private int getK() {
+        return k;
+    }
+
+    private List getClusterList() {
+        return clusterList;
+    }
+
+    private void addPointToList(Point p) {
+        pointList.add(p);
+    }
+
+    private List<Point> getPointList() {
+        return pointList;
+    }
+
+
     public static void main(String[] args) {
 
+        // Ensure correct number of arguments are passed
         if (args.length != 2) {
             System.err.println("Usage: java kmeans <k number> <filename.txt>");
             System.exit(1);
         }
 
-        // Initialize k and URL with arguments passed
-        int k = Integer.parseInt(args[0]);
-        URL url = ClassLoader.getSystemClassLoader().getResource(args[1]);
+        KMeans kmeans = new KMeans(args[0], args[1]);
 
+        // Read file passed in via arguments
         try {
-            Scanner scan = new Scanner(new File(url.getFile()));
+            Scanner scan = new Scanner(new File(kmeans.getFileString()));
             scan.useDelimiter("\n");
 
+            // Parse the file and add points to the list of points
             while (scan.hasNext()) {
-                System.out.println(scan.next());
+                // Split the line by white space between values
+                String[] coordinates = scan.next().split("\\s+");
+                int x = Integer.parseInt(coordinates[0]);
+                int y = Integer.parseInt(coordinates[1]);
+                Point p = new Point(x, y);
+                kmeans.addPointToList(p);
             }
             scan.close();
-
         } catch (FileNotFoundException e) {
             System.err.println("Exception thrown: " + e);
         }
+
+        List<Point> kPointList = kmeans.getPointList();
+        kPointList.forEach((point)-> {
+            System.out.println("x: " + point.getX() + " y: " + point.getY());
+        });
+
     }
 }
---
+++ src/Point.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by wally on 2/21/17.
+ */
+public class Point {
+}
