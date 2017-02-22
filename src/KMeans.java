a9b612be63f90bea50ad57f4b64f90d8a10d9bf0
Lyle <denmanlp@gmail.com>
2017-02-21 18:24:32 -0600

    Add clusterArray to associate points with a cluster during instantation

--- src/KMeans.java
+++ src/KMeans.java
@@ -10,15 +10,46 @@ public class KMeans {
     private int k;
     private URL fileURL;
     private List<Point> pointList;
-    private List clusterList;
+    private Cluster[] clusterArray;
 
     private KMeans(String k, String fileName) {
         this.k = Integer.parseInt(k);
-        this.fileURL = ClassLoader.getSystemClassLoader().getResource(fileName);
+        URL url = ClassLoader.getSystemClassLoader().getResource(fileName);
         this.pointList = new ArrayList<>();
-        this.clusterList = new ArrayList();
-    }
+        this.clusterArray = new Cluster[this.k + 1];
 
+        // Create k clusters
+        for (int i = 0; i < this.k; i++) {
+            Cluster c = new Cluster();
+            clusterArray[i] = c;
+        }
+
+        if (url != null) {
+            // Read file passed in
+            try {
+                Scanner scan = new Scanner(new File(url.getFile()));
+                scan.useDelimiter("\n");
+
+                // Parse the file and add points to the list of points with a randomly set cluster association
+                while (scan.hasNext()) {
+                    // Split the line by white space between values
+                    String[] coordinates = scan.next().split("\\s+");
+                    int x = Integer.parseInt(coordinates[0]);
+                    int y = Integer.parseInt(coordinates[1]);
+                    int randomVal = (int)Math.floor(Math.random() * this.k);
+                    Point p = new Point(x, y, randomVal + 1);
+                    this.addPointToList(p);
+                    clusterArray[randomVal].addPointToList(p);
+                }
+                scan.close();
+            } catch (FileNotFoundException | NullPointerException e) {
+                System.err.println("File not found.");
+                e.printStackTrace();
+            }
+        } else {
+            System.err.println("Filename could not generate path url");
+        }
+    }
     private String getFileString() {
         return fileURL.getFile();
     }
@@ -27,8 +58,8 @@ public class KMeans {
         return k;
     }
 
-    private List getClusterList() {
-        return clusterList;
+    private Cluster[] getClusterArray() {
+        return clusterArray;
     }
 
     private void addPointToList(Point p) {
@@ -39,9 +70,7 @@ public class KMeans {
         return pointList;
     }
 
-
     public static void main(String[] args) {
-
         // Ensure correct number of arguments are passed
         if (args.length != 2) {
             System.err.println("Usage: java kmeans <k number> <filename.txt>");
@@ -50,29 +79,16 @@ public class KMeans {
 
         KMeans kmeans = new KMeans(args[0], args[1]);
 
-        // Read file passed in via arguments
-        try {
-            Scanner scan = new Scanner(new File(kmeans.getFileString()));
-            scan.useDelimiter("\n");
-
-            // Parse the file and add points to the list of points
-            while (scan.hasNext()) {
-                // Split the line by white space between values
-                String[] coordinates = scan.next().split("\\s+");
-                int x = Integer.parseInt(coordinates[0]);
-                int y = Integer.parseInt(coordinates[1]);
-                Point p = new Point(x, y);
-                kmeans.addPointToList(p);
-            }
-            scan.close();
-        } catch (FileNotFoundException e) {
-            System.err.println("Exception thrown: " + e);
-        }
-
         List<Point> kPointList = kmeans.getPointList();
         kPointList.forEach((point)-> {
-            System.out.println("x: " + point.getX() + " y: " + point.getY());
+            System.out.println("x: " + point.getX() + " y: " + point.getY() + " cluster: " + point.getClusterGroupNum());
         });
+
+        Cluster[] clusters = kmeans.getClusterArray();
+        System.out.println(clusters.length);
+        for (Cluster c : clusters) {
+            System.out.println("" + c);
+        }
 
     }
 }
