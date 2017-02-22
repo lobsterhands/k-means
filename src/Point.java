ca67549f6165a1fee63d77c15ed4f804e089039e
Lyle Denman <denmanlp@gmail.com>
2017-02-22 17:32:24 -0600

    Change point coordinates to doubles for storing Euclidean distances

--- src/Point.java
+++ src/Point.java
@@ -2,21 +2,23 @@
  * Created by wally on 2/21/17.
  */
 public class Point {
-    private int x;
-    private int y;
+    private double x;
+    private double y;
+
     private int clusterGroupNum;
 
-    public Point(int x, int y, int clusterGroupNum) {
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
-        this.clusterGroupNum = clusterGroupNum;
+        this.clusterGroupNum = 0;
     }
 
-    public int getX() {
+
+    public double getX() {
         return x;
     }
 
-    public int getY() {
+    public double getY() {
         return y;
     }
 
@@ -24,15 +26,19 @@ public class Point {
         return clusterGroupNum;
     }
 
+    public void setClusterGroupNum(int clusterGroupNum) {
+        this.clusterGroupNum = clusterGroupNum;
+    }
+
     public double calcDistanceToCentroid(Point centroid) {
-        int xDifference = centroid.getX() - x;
-        int yDifference = centroid.getY() - y;
+        double xDifference = centroid.getX() - x;
+        double yDifference = centroid.getY() - y;
         return Math.sqrt((Math.pow(xDifference, 2) + Math.pow(yDifference, 2)));
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append(String.format("x: %-3s  y: %-3s cluster: %s\n", this.x, this.y, clusterGroupNum));
+        sb.append(String.format("x: %-3s  y: %-3s  cluster: %s", x, y, clusterGroupNum));
         return sb.toString();
     }}
