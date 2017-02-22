e9bf58df9bddd8d1454327068b3e75ec8c7a11b4
Lyle Denman <denmanlp@gmail.com>
2017-02-22 17:34:08 -0600

    Add logic for calculating and updating centroid positiions and distance of centroid changes

--- src/Cluster.java
+++ src/Cluster.java
@@ -11,6 +11,12 @@ public class Cluster {
     private Point centroid;
     private List<Point> pointList;
 
+    private double centroidDistanceChange;
+
+    public double getCentroidDistanceChange() {
+        return centroidDistanceChange;
+    }
+
     public Cluster() {
         this.clusterID = ++clustersTotal;
         this.pointList = new ArrayList<>();
@@ -29,7 +35,11 @@ public class Cluster {
     }
 
     public boolean isEmpty() {
-        return this.pointList.size() == 0;
+        return pointList.size() == 0;
+    }
+
+    public int getSize() {
+        return pointList.size();
     }
 
     public Point getCentroid() {
@@ -40,18 +50,51 @@ public class Cluster {
         this.centroid = centroid;
     }
 
+    public void calculateInitialCentroid() {
+        double sumX = 0;
+        double sumY = 0;
+        int nPoints = 0;
+        for (Point p : pointList) {
+            sumX += p.getX();
+            sumY += p.getY();
+            nPoints++;
+        }
+        double avgX = sumX / nPoints;
+        double avgY = sumY / nPoints;
+        this.centroid = new Point(avgX, avgY);
+    }
+
+    public void updateCentroid() {
+        Point oldCentroid = centroid;
+        double sumX = 0;
+        double sumY = 0;
+        int nPoints = 0;
+        for (Point p : pointList) {
+            sumX += p.getX();
+            sumY += p.getY();
+            nPoints++;
+        }
+        double avgX = sumX / nPoints;
+        double avgY = sumY / nPoints;
+        this.centroid = new Point(avgX, avgY);
+
+        centroidDistanceChange = Math.sqrt((Math.pow((avgX - oldCentroid.getX()), 2) + Math.pow(avgY - oldCentroid.getY(), 2)));
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append(String.format("ClusterID: %s\n", this.clusterID));
+        sb.append(String.format("ClusterID: %s (Size: %s)\n", clusterID, pointList.size()));
+        if (centroid != null){
+            sb.append(String.format("Centroid: x: %-3s y: %-3s\n", centroid.getX(), centroid.getY()));
+        } else {
+            sb.append("Centroid has not been placed\n");
+        }
         if (this.pointList.isEmpty()) {
             sb.append("This cluster is empty.");
         } else {
             for (Point p : pointList) {
                 sb.append(String.format("x: %s y: %s\n",  p.getX(), p.getY()));
-            }
-            if (centroid != null) {
-                sb.append(String.format("Centroid: x: %-3s y: %-3s", centroid.getX(), centroid.getY()));
             }
         }
         return sb.toString();
