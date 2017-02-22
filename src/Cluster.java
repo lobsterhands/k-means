89296a818ef79adbeaf1ec44a0900ea65523bd65
Lyle <denmanlp@gmail.com>
2017-02-21 18:23:18 -0600

    Add pointList to track associations for each iteration; Override toString() in progress.

--- src/Cluster.java
+++ src/Cluster.java
@@ -1,5 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by wally on 2/21/17.
  */
 public class Cluster {
+
+    private static int clustersTotal = 0;
+
+    private int clusterID;
+
+    private List<Point> pointList;
+
+    public Cluster() {
+        this.clusterID = ++clustersTotal;
+        this.pointList = new ArrayList<>();
+    }
+
+    public int getClusterID() {
+        return clusterID;
+    }
+
+    public void addPointToList(Point p) {
+        pointList.add(p);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("ClusterID: %s\n", this.clusterID));
+        this.pointList.forEach((point) -> {
+            sb.append(String.format("x: %s y: %s\n",  point.getX(), point.getY()));
+        });
+        return sb.toString();
+    }
+
 }
