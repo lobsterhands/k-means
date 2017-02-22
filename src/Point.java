1294feadd5cc0c1fd28f72af5f566d68c0016d49
Lyle Denman <denmanlp@gmail.com>
2017-02-22 13:31:21 -0600

    Add Euclidean distance calculator

--- src/Point.java
+++ src/Point.java
@@ -23,4 +23,16 @@ public class Point {
     public int getClusterGroupNum() {
         return clusterGroupNum;
     }
-}
+
+    public double calcDistanceToCentroid(Point centroid) {
+        int xDifference = centroid.getX() - x;
+        int yDifference = centroid.getY() - y;
+        return Math.sqrt((Math.pow(xDifference, 2) + Math.pow(yDifference, 2)));
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("x: %-3s  y: %-3s cluster: %s\n", this.x, this.y, clusterGroupNum));
+        return sb.toString();
+    }}
